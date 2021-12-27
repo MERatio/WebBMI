bb5497568a677c9662e34452686ae1eb2fc4b950
Mark Edward Ratio <markedwardratio@gmail.com>
2021-12-27 12:13:33 +0800

    Add feature to calculate bmi with hard-coded params

---
+++ bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBMI = (heightCm: number, weightKg: number): string => {
+	const meter = heightCm / 100;
+	const bmi = weightKg / Math.pow(meter, 2);
+
+	if (bmi < 18.5) {
+		return 'Underweight';
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		return 'Normal (healthy weight)';
+	} else if (bmi >= 25 && bmi <= 29.9) {
+		return 'Overweight';
+	} else if (bmi >= 30) {
+		return 'Obese';
+	}
+};
+
+console.log(calculateBMI(180, 74));
