8ac9161a51a1a54a9ace91b05376d32234443f81
Mark Edward Ratio <markedwardratio@gmail.com>
2021-12-27 15:36:12 +0800

    Update bmiCalculator to accept command-line arguments

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,3 +1,25 @@
+interface BmiArgs {
+	heightCm: number;
+	weightKg: number;
+}
+
+const parseBmiArguments = (args: Array<string>): BmiArgs => {
+	if (args.length < 4) {
+		throw new Error('Not enough arguments.');
+	} else if (args.length > 4) {
+		throw new Error('Too many arguments.');
+	}
+
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			heightCm: Number(args[2]),
+			weightKg: Number(args[3]),
+		};
+	} else {
+		throw new Error('Arguments are not numbers.');
+	}
+};
+
 const calculateBMI = (heightCm: number, weightKg: number): string => {
 	const meter: number = heightCm / 100;
 	const bmi: number = weightKg / Math.pow(meter, 2);
@@ -13,4 +35,13 @@ const calculateBMI = (heightCm: number, weightKg: number): string => {
 	}
 };
 
-console.log(calculateBMI(180, 74));
+try {
+	const { heightCm, weightKg } = parseBmiArguments(process.argv);
+	console.log(calculateBMI(heightCm, weightKg));
+} catch (error: unknown) {
+	let errorMessage: string = 'Something went wrong.';
+	if (error instanceof Error) {
+		errorMessage += ` Error: ${error.message}`;
+	}
+	console.log(errorMessage);
+}
